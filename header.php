6cc65e35acd14beabc420b5fbdb492c7d066589b
CFVerdier <73821879+CFVerdier@users.noreply.github.com>
2021-04-07 12:40:36 +0200

    updated structure

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+  echo'
+  <div class="header">
+    <h1>Dr. Ir. Cees Ferdindand Verdier</h1>
+  </div>
+
+  <!--Navigation bar-->
+  <div class="navbar">
+    <a href="#">Software</a>
+    <a href="publications.html">Publications</a>
+    <a href="#">Portfolio</a>
+    <a href="#">Curriculum vitae</a>
+    <a href="index.html">Home</a>
+  </div>
+  '
+ ?>
